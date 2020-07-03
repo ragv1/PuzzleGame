44bf7964974157d1db0442dd43684a6b21c08ec2
Ramon German Ventura <rgerman@suedfarma.local>
2020-07-03 16:07:25 -0400

    Fixed ISSUE #2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyByR2UiHUy4s2xzPfHpabi4QKAuhligoN8",
+    authDomain: "test-53118.firebaseapp.com",
+    databaseURL: "https://test-53118.firebaseio.com",
+    projectId: "test-53118",
+    storageBucket: "test-53118.appspot.com",
+    messagingSenderId: "383890048846",
+    appId: "1:383890048846:web:b4c89494955cfc34b1c01f",
+    measurementId: "G-1ES78NZDV9"
+  }
 };
